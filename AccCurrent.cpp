1514f1a707a115be3f746ac52d379a80b5c6b771
Ginger0verlord <13447557@students.lincoln.ac.uk>
2017-03-03 12:19:35 +0000

    Update AccCurrent.cpp

--- AccCurrent.cpp
+++ AccCurrent.cpp
@@ -8,3 +8,15 @@ AccCurrent::~AccCurrent()
 {
 }
 
+void Account::Deposit(int val) {
+	if (val >= 200)
+  { val = val * 0.005}
+  
+	Balance += val;
+	int pounds, pence;
+	pounds = float(val) / 100;
+	pence = val - pounds*100;
+	cout << pounds <<"."<< pence << " Deposited" << endl;
+
+	cout << "Transaction Complete" << endl;
+}
