8ee11707d76155a99e61ad45a3cbfa1ca3079c30
Ginger0verlord <13447557@students.lincoln.ac.uk>
2017-03-03 12:27:38 +0000

    Update AccSavings.cpp

--- AccSavings.cpp
+++ AccSavings.cpp
@@ -8,3 +8,18 @@ AccSavings::AccSavings()
 AccSavings::~AccSavings()
 {
 }
+
+void AccSavings::Deposit(int val) {
+	if (val >= 100000)
+  { val = val * 0.012}
+  else
+  {val = val * 0.008}
+  
+	Balance += val;
+	int pounds, pence;
+	pounds = float(val) / 100;
+	pence = val - pounds*100;
+	cout << pounds <<"."<< pence << " Deposited" << endl;
+
+	cout << "Transaction Complete" << endl;
+}
